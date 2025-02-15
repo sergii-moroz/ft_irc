91e1846b9745da1c6979f9516de99e6a8950c4e4
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-02-15 16:13:39 +0100

    utils: add static validation functions • add validatePort function • add validatePass function • add helper functions trim, isInteger, strToInteger

--- utils/Utils.cpp
+++ utils/Utils.cpp
@@ -6,7 +6,7 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 15:00:16 by smoroz            #+#    #+#             */
-/*   Updated: 2025/02/15 15:00:16 by smoroz           ###   ########.fr       */
+/*   Updated: 2025/02/15 16:10:26 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,4 +48,85 @@ Utils &	Utils::operator=(Utils const & rhs)
 		// do nothing
 	}
 	return (*this);
+}
+
+// ==========================================
+// Static functions
+// ==========================================
+
+std::string	Utils::trim(std::string const & s)
+{
+	size_t	start = s.find_first_not_of("\f\n\r\t\v ");
+	if (start == std::string::npos)
+		return (s); // no content
+	size_t	end	= s.find_last_not_of("\f\n\r\t\v ");
+	size_t	range = end - start + 1;
+	return (s.substr(start, range));
+}
+
+bool	Utils::isInteger(std::string const & s)
+{
+	std::istringstream iss(Utils::trim(s));
+	int	i;
+	iss >> i;
+	return (!iss.fail() && iss.eof());
+}
+
+int	Utils::strToInteger(std::string const & s)
+{
+	std::istringstream	iss(Utils::trim(s));
+	int	i;
+	iss >> i;
+	return (i);
+}
+
+int	Utils::validatePort(char const *arg)
+{
+	if (!Utils::isInteger(arg))
+	{
+		std::cerr << "ERROR: First argument is not an integer!" << std::endl;
+		// Server::usage();
+		return (-1);
+	}
+
+	int port = Utils::strToInteger(arg);
+	if (port < 0)
+	{
+		std::cerr << "ERROR: Port number should be a positive value." << std::endl;
+		return (-1);
+	}
+
+	if (port < 1024)
+	{
+		std::cerr << "ERROR: Port numbers from 0 to 1023 are reserved for common TCP/IP applications." << std::endl;
+		return (-1);
+	}
+
+	return (port);
+}
+
+std::string	Utils::validatePass(char const *arg)
+{
+	std::string	pass(arg);
+	std::cout << "\"" << pass << "\"" << std::endl;
+
+	if (pass.empty())
+	{
+		std::cerr << "ERROR: Password cannot be empty!" << std::endl;
+		return (std::string(""));
+	}
+
+	if (pass.length() < 6) {
+		std::cerr << "ERROR: Password must be at least 6 characters long!" << std::endl;
+		return (std::string(""));
+	}
+
+	size_t	pos = pass.find_first_of("\f\n\r\t\v ");
+	if (pos != std::string::npos)
+	{
+		std::cerr << "ERROR: Password cannot contain whitespace characters!" << std::endl;
+		return (std::string(""));
+	}
+
+	return (pass);
 }
--- utils/Utils.hpp
+++ utils/Utils.hpp
@@ -6,7 +6,7 @@
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 14:58:45 by smoroz            #+#    #+#             */
-/*   Updated: 2025/02/15 14:58:45 by smoroz           ###   ########.fr       */
+/*   Updated: 2025/02/15 16:10:14 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,10 +20,18 @@
 class Utils
 {
 	public:
-		Utils();
 		~Utils();
+		static int			validatePort(char const *arg);
+		static std::string	validatePass(char const *arg);
+
+	private:
+		Utils();
 		Utils(Utils const &);
 		Utils &	operator=(Utils const &);
+
+		static std::string	trim(std::string const & s);
+		static bool			isInteger(std::string const & s);
+		static int			strToInteger(std::string const & s);
 };
 
 #endif
