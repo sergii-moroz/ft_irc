1c0ac9894b45d5cbf75ac937e608ef6c58be1162
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-02-16 15:36:43 +0100

    cap: refactoring CAP command • create handleNumeric.cpp • add errNeedMoreParams function to handle ERR_NEEDMOREPARAMS (461)

--- commandHandler/handleNumeric.cpp
+++ commandHandler/handleNumeric.cpp
@@ -1,28 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   handleCAP.cpp                                      :+:      :+:    :+:   */
+/*   handleNumeric.cpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/16 12:34:55 by smoroz            #+#    #+#             */
-/*   Updated: 2025/02/16 12:34:55 by smoroz           ###   ########.fr       */
+/*   Created: 2025/02/16 12:56:32 by smoroz            #+#    #+#             */
+/*   Updated: 2025/02/16 12:56:32 by smoroz           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "CommandHandler.hpp"
 
-void	CommandHandler::handleCAP(int sd, Command const & cmd)
+
+
+// ==========================================
+// ERR_NEEDMOREPARAMS (461)
+// ==========================================
+
+std::string	CommandHandler::errNeedMoreParams(std::string const & serverName, std::string const & command) const
 {
-	if (cmd.isParamEmpty() || !cmd.hasParamAtPos(0, 0) || !cmd.hasParamAtPos(1, 0))
-	{
-		std::string	msg = errNeedMoreParams(_server->getName(), cmd.getName());
-		_server->sendData(sd, msg);
-		_server->clearClient(sd);
-	}
-	else if(cmd.hasParamAtPos("LS", 0, 0))
-	{
-		std::string	msg = ":" + _server->getName() + " CAP * LS :\r\n";
-		_server->sendData(sd, msg);
-	}
+	return (":" + serverName + " 461 " + command + " :Not enough parameters\r\n");
 }
