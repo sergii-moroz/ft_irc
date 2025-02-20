536b748e3c2ecf3d6fc88ba6e5853c11cfa501ce
Sergii Moroz <smoroz@student.42heilbronn.de>
2025-02-20 13:31:09 +0100

    channel: add attributes: _mode, _topic and corresponding getters setters

--- channel/Channel.cpp
+++ channel/Channel.cpp
@@ -11,37 +11,58 @@
 /******************************************************************************/
 
 #include "Channel.hpp"
-#include "Server.hpp"
-#include <iostream> 
 
-Channel::Channel(const std::string &name)
-: _name(name)
+Channel::Channel() : _name(""), _topic("unknown"), _mode(0)
 {
+	std::cout << "Channel: Default constructor called" << std::endl;
+}
+
+Channel::Channel(const std::string &name) : _name(name), _topic("unknown"), _mode(0)
+{
+	std::cout << "Channel: Named constructor called" << std::endl;
 }
 
 Channel::Channel(const Channel &other)
-: _name(other._name), _userFds(other._userFds)
 {
+	*this = other;
+}
+
+Channel::~Channel()
+{
+	std::cout << "Channel: Destructor called" << std::endl;
 }
 
 Channel &Channel::operator=(const Channel &other)
 {
-    if (this != &other)
-    {
-        _name = other._name;
-        _userFds = other._userFds;
-    }
-    return *this;
-}
-
-
-Channel::~Channel()
-{
+	if (this != &other)
+	{
+		std::cout << "Channel: Assignment operator called" << std::endl;
+		_name = other._name;
+		_topic = other._topic;
+		_mode = other._mode;
+		_userFds = other._userFds;
+	}
+	return (*this);
 }
 
 const std::string & Channel::getName() const
 {
-	return _name;
+	return (_name);
+}
+
+std::string const &	Channel::getTopic() const
+{
+	return (_topic);
+}
+
+void	Channel::setName(std::string const & name)
+{
+	_name = name;
+}
+
+void	Channel::setTopic(std::string const & topic)
+{
+	_topic = topic;
 }
 
 void Channel::addUser(int userFd)
@@ -59,17 +80,17 @@ bool Channel::hasUser(int userFd) const
 	return (_userFds.find(userFd) != _userFds.end());
 }
 
-void Channel::broadcast(Server &server, const std::string &senderNick,
-						const std::string &message, int excludeFd)
-{
-	std::string fullMsg = ":" + senderNick 
-						+ " PRIVMSG " + _name 
-						+ " :" + message 
-						+ "\r\n";
-	for (std::set<int>::iterator it = _userFds.begin(); it != _userFds.end(); ++it)
-	{
-		if (*it == excludeFd)
-			continue; 
-		server.sendData(*it, fullMsg);
-	}
-}
+// void Channel::broadcast(Server &server, const std::string &senderNick,
+// 						const std::string &message, int excludeFd)
+// {
+// 	std::string fullMsg = ":" + senderNick
+// 						+ " PRIVMSG " + _name
+// 						+ " :" + message
+// 						+ "\r\n";
+// 	for (std::set<int>::iterator it = _userFds.begin(); it != _userFds.end(); ++it)
+// 	{
+// 		if (*it == excludeFd)
+// 			continue;
+// 		server.sendData(*it, fullMsg);
+// 	}
+// }
