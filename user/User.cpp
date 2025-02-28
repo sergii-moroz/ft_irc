150a65186a583e8053305c5d8ddb40a0c974ec59
Oleksandr Anokhin <olanokhi@1-A-24.42heilbronn.de>
2025-02-28 11:06:27 +0100

    complete User.cpp with proper method's implementation

--- user/User.cpp
+++ user/User.cpp
@@ -3,67 +3,57 @@
 /*                                                        :::      ::::::::   */
 /*   User.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: olanokhi <olanokhi@42heilbronn.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 12:11:32 by smoreron          #+#    #+#             */
-/*   Updated: 2025/02/17 16:00:18 by smoroz           ###   ########.fr       */
+/*   Updated: 2025/02/27 14:00:07 by olanokhi         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "User.hpp"
 
-User::User() : _fd(-1), _registered(false), _passOK(false),
-	_nickname("*"), _username(""), _realname("") {}
+// Constructors
+User::User() : _fd(-1), _status(0), _nickname("*"), _username(""), _realname("") {}
 
-User::User(int fd) : _fd(fd), _registered(false), _passOK(false),
-	_nickname("*"), _username(""), _realname("") {}
+User::User(int fd) : _fd(fd), _status(0), _nickname("*"), _username(""), _realname("") {}
 
 User::User(User const & other)
 {
 	*this = other;
 }
 
-// ==========================================
-// Destructor
-// ==========================================
-
 User::~User() {}
-
-// ==========================================
-// Assignment operator=()
-// ==========================================
 
 User &	User::operator=(User const & other)
 {
 	if (this != &other)
 	{
 		_fd = other.getFd();
-		_registered = other.isRegistered();
-		_passOK = other.isPassOK();
+		_status = other._status;
 		_nickname = other.getNickname();
 		_username = other.getUsername();
 		_realname = other.getRealname();
+		_buffer = other._buffer;
+		_joinedChannels = other._joinedChannels;
 	}
 	return (*this);
 }
 
-// ==========================================
-// Getters / Setters
-// ==========================================
+// Getters
 
 int	User::getFd(void) const
 {
 	return (_fd);
 }
 
-bool	User::isRegistered(void) const
+char	User::getStatus() const
 {
-	return (_registered);
+	return (_status);
 }
 
-bool	User::isPassOK(void) const
+bool	User::getStatus(char status) const
 {
-	return (_passOK);
+	return ((_status >> status) & 1);
 }
 
 std::string const &	User::getNickname(void) const
@@ -81,27 +71,16 @@ std::string const &	User::getRealname(void) const
 	return (_realname);
 }
 
-std::string	User::getNextCommand(void)
+std::set<Channel *> const &	User::getJoinedChannels(void) const
 {
-	size_t	pos = _buffer.find("\r\n");
-
-	if (pos == std::string::npos)
-		return ("");
-
-	std::string	s = _buffer.substr(0, pos + 2);
-	_buffer.erase(0, pos + 2);
-
-	return (s);
+	return (_joinedChannels);
 }
 
-void	User::setRegistered(bool val)
+// Setters
+void	User::setStatus(char key, bool value)
 {
-	_registered = val;
-}
-
-void	User::setPassOK(bool val)
-{
-	_passOK = val;
+	if (((_status >> key) & 1) != value)
+		_status ^= (1 << key);
 }
 
 void	User::setNickname(std::string const & nick)
@@ -119,11 +98,34 @@ void	User::setRealname(std::string const & real)
 	_realname = real;
 }
 
+// Methods
 void	User::addToBuffer(std::string const & msg)
 {
 	_buffer += msg;
 }
 
+void	User::joinChannel(Channel * channel)
+{
+	_joinedChannels.insert(channel);
+}
+
+void	User::leaveChannel(Channel * channel)
+{
+	_joinedChannels.erase(channel);
+}
+
+std::string	User::getNextCommand(void)
+{
+	size_t	pos = _buffer.find("\r\n");
+
+	if (pos == std::string::npos)
+		return ("");
+
+	std::string	s = _buffer.substr(0, pos + 2);
+	_buffer.erase(0, pos + 2);
+
+	return (s);
+}
 // ==========================================
 // Overload operator<<
 // ==========================================
@@ -131,12 +133,23 @@ void	User::addToBuffer(std::string const & msg)
 std::ostream &	operator<<(std::ostream & out, User const & ref)
 {
 	out << "User: { " << std::endl
-		<< std::string(3, ' ') << "nickname: " << ref.getNickname() << std::endl
-		<< std::string(3, ' ') << "username: " << ref.getUsername() << std::endl
-		<< std::string(3, ' ') << "realname: " << ref.getRealname() << std::endl
-		<< std::string(3, ' ') << "sock desc: " << ref.getFd() << std::endl
-		<< std::string(3, ' ') << "registered: " << ref.isRegistered() << std::endl
-		<< std::string(3, ' ') << "authenticated: " << ref.isPassOK() << std::endl
-		<< "}";
+		<< std::string(2, ' ') << "nickname: " << ref.getNickname() << std::endl
+		<< std::string(2, ' ') << "username: " << ref.getUsername() << std::endl
+		<< std::string(2, ' ') << "realname: " << ref.getRealname() << std::endl
+		<< std::string(2, ' ') << "sock desc: " << ref.getFd() << std::endl
+		<< std::string(2, ' ') << "is CAP LS setted: " << ref.getStatus(CAP_LS) << std::endl
+		<< std::string(2, ' ') << "is CAP END setted: " << ref.getStatus(CAP_END) << std::endl
+		<< std::string(2, ' ') << "is authenticated: " << ref.getStatus(PASSWORD) << std::endl
+		<< std::string(2, ' ') << "is Nick Setted: " << ref.getStatus(NICK) << std::endl
+		<< std::string(2, ' ') << "is User Setted: " << ref.getStatus(USER) << std::endl
+		<< std::string(2, ' ') << "is registered: " << ref.getStatus(REGISTERED) << std::endl
+		<< std::string(2, ' ') << "joined channel: " << ref.getJoinedChannels().size() << std::endl
+		<< std::string(2, ' ') << "{ " << std::endl << std::string(4, ' ');
+		for (std::set<Channel *>::const_iterator it = ref.getJoinedChannels().begin(); it != ref.getJoinedChannels().end(); it++)
+		{
+			out << (*it)->getName() << " ";
+		}
+		out << std::string(2, ' ') << "}" << std::endl;
+		out << std::endl << "}";
 	return (out);
 }
