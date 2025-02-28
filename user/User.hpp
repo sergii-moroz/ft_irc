7714367b17380ab23bc1322e32480351e6bdf433
Oleksandr Anokhin <olanokhi@1-A-24.42heilbronn.de>
2025-02-28 11:05:58 +0100

    complete and fix User.hpp with proper names for variables and methods to match with new Channel class

--- user/User.hpp
+++ user/User.hpp
@@ -3,55 +3,81 @@
 /*                                                        :::      ::::::::   */
 /*   User.hpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smoroz <smoroz@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*   By: olanokhi <olanokhi@42heilbronn.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/25 12:04:09 by smoreron          #+#    #+#             */
-/*   Updated: 2025/02/17 16:00:24 by smoroz           ###   ########.fr       */
+/*   Updated: 2025/02/27 14:00:00 by olanokhi         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #ifndef USER_HPP
 # define USER_HPP
 
+# include <set>
 # include <string>
 # include <iostream>
 
 # include "Utils.hpp"
+# include "Channel.hpp"
 
-# define USERLEN 18
+# define CAP_LS			0
+# define CAP_END		1
+# define PASSWORD		2
+# define NICK			3
+# define USER			4
+# define REGISTERED		5
+# define USERLEN		18
+# define END_REG		31 // 2^4 + 2^3 + 2^2 + 2 + 1
+
+class Channel;
 
 class User
 {
 	public:
+		// Constructors and destructor
 		User();
 		User(User const &);
 		User(int fd);
 		~User();
 		User &		operator=(User const &);
 
-		int			getFd(void) const;
-		bool		isRegistered(void) const;
-		bool		isPassOK(void) const;
+		// Getters
+		int					getFd(void) const;
+		char				getStatus() const;
+		bool				getStatus(char) const;
 		std::string const &	getNickname(void) const;
 		std::string const &	getUsername(void) const;
 		std::string const &	getRealname(void) const;
-		std::string	getNextCommand(void);
+		std::set<Channel *> const &	getJoinedChannels() const;
 
-		void		setRegistered(bool val);
-		void		setPassOK(bool val);
-		void		setNickname(std::string const &);
-		void		setUsername(std::string const &);
-		void		setRealname(std::string const &);
-		void		addToBuffer(std::string const &);
+		// Setters
+		void				setStatus(char, bool);
+		void				setNickname(std::string const &);
+		void				setUsername(std::string const &);
+		void				setRealname(std::string const &);
+
+		// Methods
+		void				addToBuffer(std::string const &);
+		void				joinChannel(Channel *);
+		void				leaveChannel(Channel *);
+		std::string			getNextCommand(void);
+
+		// Deprecated
+		// bool				isRegistered(void) const;
+		// bool				isPassOK(void) const;
+		// void				setRegistered(bool val);
+		// void				setPassOK(bool val);
 
 	private:
-		int			_fd;
-		bool		_registered;
-		bool		_passOK;
-		std::string	_nickname;
-		std::string	_username;
-		std::string	_realname;
-		std::string	_buffer;
+		int					_fd;
+		char				_status;
+		std::string			_nickname;
+		std::string			_username;
+		std::string			_realname;
+		std::string			_buffer;
+		std::set<Channel *>	_joinedChannels;
+		// bool				_registered;
+		// bool				_passOK;
 };
 
 std::ostream &	operator<<(std::ostream &, User const &);
