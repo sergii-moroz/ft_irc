0ffad2128fbf79bdb87af268062d8a0cc6918d90
Oleksandr Anokhin <olanokhi@1-A-24.42heilbronn.de>
2025-02-28 11:57:54 +0100

    update TOPIC command implementation to fit new Channel, User and Server classes

--- commandHandler/handleTOPIC.cpp
+++ commandHandler/handleTOPIC.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   handleTOPIC.cpp                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smoreron <smoreron@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: olanokhi <olanokhi@42heilbronn.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/25 22:39:34 by smoreron          #+#    #+#             */
-/*   Updated: 2025/02/26 02:22:17 by smoreron         ###   ########.fr       */
+/*   Updated: 2025/02/27 14:33:34 by olanokhi         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
@@ -18,10 +18,10 @@ void CommandHandler::handleTOPIC(int sd, Command const & cmd)
 	User &user = _server->getUser(sd);
 	std::string nick = user.getNickname();
 
-	
+
 	if (cmd.isParamEmpty() || !cmd.hasParamAtPos(0, 0))
 	{
-		
+
 		std::string errMsg = ":" + _server->getName() + " 461 "
 							 + nick + " TOPIC :Not enough parameters\r\n";
 		_server->sendData(sd, errMsg);
@@ -33,33 +33,33 @@ void CommandHandler::handleTOPIC(int sd, Command const & cmd)
 
 	if (!channel)
 	{
-		
+
 		std::string errMsg = ":" + _server->getName() + " 403 "
-							 + nick + " " + channelName 
+							 + nick + " " + channelName
 							 + " :No such channel\r\n";
 		_server->sendData(sd, errMsg);
 		return;
 	}
 
-	
-	if (!channel->hasUser(sd))
+
+	if (!channel->isUser(&user))
 	{
-		
+
 		std::string errMsg = ":" + _server->getName() + " 442 "
-							 + nick + " " + channelName 
+							 + nick + " " + channelName
 							 + " :You're not on that channel\r\n";
 		_server->sendData(sd, errMsg);
 		return;
 	}
 
-	
+
 	if (cmd.getParameters().size() < 2 || !cmd.hasParamAtPos(1, 0))
 	{
-		
+
 		std::string currentTopic = channel->getTopic();
 		if (currentTopic.empty() || currentTopic == "unknown")
 		{
-			
+
 			std::string msg = ":" + _server->getName() + " 331 "
 							  + nick + " " + channelName
 							  + " :No topic is set\r\n";
@@ -67,7 +67,7 @@ void CommandHandler::handleTOPIC(int sd, Command const & cmd)
 		}
 		else
 		{
-			
+
 			std::string msg = ":" + _server->getName() + " 332 "
 							  + nick + " " + channelName
 							  + " :" + currentTopic + "\r\n";
@@ -77,9 +77,9 @@ void CommandHandler::handleTOPIC(int sd, Command const & cmd)
 	else
 	{
 
-		if ((channel->_mode & MODE_TOPIC_LOCK) && !channel->isOperator(sd))
+		if ((channel->getMode(TOPIC_MODE)) && !channel->isOperator(&user))
 		{
-			
+
 			std::string errMsg = ":" + _server->getName() + " 482 "
 								 + nick + " " + channelName
 								 + " :You're not channel operator\r\n";
@@ -87,15 +87,15 @@ void CommandHandler::handleTOPIC(int sd, Command const & cmd)
 			return;
 		}
 
-		
+
 		std::string newTopic = cmd.getParamAtPos(1, 0);
 		channel->setTopic(newTopic);
 
-		
+
 		std::string topicMsg = ":" + nick + "!" + user.getUsername()
 							   + "@" + _server->getName()
-							   + " TOPIC " + channelName 
+							   + " TOPIC " + channelName
 							   + " :" + newTopic + "\r\n";
-		channel->broadcastRaw(*_server, topicMsg);
+		channel->broadcastAll(_server, topicMsg);
 	}
 }
