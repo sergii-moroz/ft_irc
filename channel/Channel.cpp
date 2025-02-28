ed4f2dbbc5b79e080b362dc399cec1743a05dd6f
Oleksandr Anokhin <olanokhi@1-A-24.42heilbronn.de>
2025-02-28 11:04:35 +0100

    complete Channel.cpp with proper method's implementation

--- channel/Channel.cpp
+++ channel/Channel.cpp
@@ -3,34 +3,23 @@
 /*                                                        :::      ::::::::   */
 /*   Channel.cpp                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: smoreron <smoreron@student.42heilbronn.    +#+  +:+       +#+        */
+/*   By: olanokhi <olanokhi@42heilbronn.de>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/18 20:51:11 by smoreron          #+#    #+#             */
-/*   Updated: 2025/02/25 22:20:55 by smoreron         ###   ########.fr       */
+/*   Updated: 2025/02/27 13:23:53 by olanokhi         ###   ########.fr       */
 /*                                                                            */
 /******************************************************************************/
 
 #include "Channel.hpp"
-#include "Server.hpp"
-#include <iostream>
-#include <cstdlib> 
 
-Channel::Channel() :
-	_name(""),
-	_topic("unknown"),
-	_mode(0),
-	_userLimit(0) 
+Channel::Channel() : _name(""), _topic(" "), _key(""), _userLimit(0),  _mode(0)
 {
-	std::cout << "Channel: Default constructor called" << std::endl;
+	// std::cout << "Channel: Default constructor called" << std::endl;
 }
 
-Channel::Channel(const std::string &name) :
-	_name(name),
-	_topic("unknown"),
-	_mode(0),
-	_userLimit(0)
+Channel::Channel(const std::string &name) : _name(name), _topic(" "), _key(""), _userLimit(0), _mode(0)
 {
-	std::cout << "Channel: Named constructor called" << std::endl;
+	// std::cout << "Channel: Named constructor called" << std::endl;
 }
 
 Channel::Channel(const Channel &other)
@@ -40,26 +29,26 @@ Channel::Channel(const Channel &other)
 
 Channel::~Channel()
 {
-	std::cout << "Channel: Destructor called" << std::endl;
+	// std::cout << "Channel: Destructor called" << std::endl;
 }
 
 Channel &Channel::operator=(const Channel &other)
 {
 	if (this != &other)
 	{
-		_name      = other._name;
-		_topic     = other._topic;
-		_mode      = other._mode;
-		_userFds   = other._userFds;
-		_operators = other._operators;
-
-		_password  = other._password;
-		_userLimit = other._userLimit;
-		_banList   = other._banList;
+		_name		= other._name;
+		_topic		= other._topic;
+		_key		= other._key;
+		_users		= other._users;
+		_operators	= other._operators;
+		_userLimit	= other._userLimit;
+		_invited	= other._invited;
+		_mode		= other._mode;
 	}
 	return (*this);
 }
 
+// Getters
 const std::string & Channel::getName() const
 {
 	return (_name);
@@ -70,120 +59,137 @@ std::string const & Channel::getTopic() const
 	return (_topic);
 }
 
-void Channel::setName(std::string const & name)
+std::string const & Channel::getKey() const
+{
+	return (_key);
+}
+
+bool Channel::getMode(char key) const
+{
+	return ((_mode >> key) & 1);
+}
+
+size_t Channel::getUserLimit() const
+{
+	return (_userLimit);
+}
+
+std::set<User *> Channel::getUsers() const
+{
+	return (_users);
+}
+
+std::set<User *> Channel::getOperators() const
+{
+	return (_operators);
+}
+
+std::set<User *> Channel::getInvitedUsers() const
+{
+	return (_invited);
+}
+
+// Setters
+void Channel::setName(std::string const &name)
 {
 	_name = name;
 }
 
-void Channel::setTopic(std::string const & topic)
+void Channel::setTopic(std::string const &topic)
 {
 	_topic = topic;
 }
 
-
-void Channel::addUser(int userFd)
+void Channel::setKey(std::string const &key)
 {
-	_userFds.insert(userFd);
-}
-
-void Channel::removeUser(int userFd)
-{
-	_userFds.erase(userFd);
-}
-
-bool Channel::hasUser(int userFd) const
-{
-	return (_userFds.find(userFd) != _userFds.end());
-}
-
-std::set<int> Channel::getUsers() const
-{
-	return _userFds;
-}
-
-void Channel::addOperator(int userFd)
-{
-	_operators.insert(userFd);
-}
-
-void Channel::removeOperator(int userFd)
-{
-	_operators.erase(userFd);
-}
-
-bool Channel::isOperator(int userFd) const
-{
-	return (_operators.find(userFd) != _operators.end());
-}
-
-void Channel::setPassword(const std::string &pass)
-{
-	_password = pass;
-}
-const std::string & Channel::getPassword() const
-{
-	return _password;
+	_key = key;
 }
 
 void Channel::setUserLimit(size_t limit)
 {
 	_userLimit = limit;
 }
-size_t Channel::getUserLimit() const
+
+void Channel::setMode(char key, bool value)
 {
-	return _userLimit;
+	if (((_mode >> key) & 1) != value)
+		_mode ^= (1 << key);
 }
 
-void Channel::addBan(const std::string &nick)
+// Resource operations
+void Channel::addUser(User *user)
 {
-	_banList.insert(nick);
-}
-void Channel::removeBan(const std::string &nick)
-{
-	_banList.erase(nick);
-}
-bool Channel::isBanned(const std::string &nick) const
-{
-	return (_banList.find(nick) != _banList.end());
+	_users.insert(user);
+	user->joinChannel(this);
 }
 
-
-void Channel::broadcast(Server &server, const std::string &senderNick,
-	const std::string &message, int excludeFd)
+void Channel::removeUser(User *user)
 {
-	std::string fullMsg = ":" + senderNick
-		+ " PRIVMSG " + _name
-		+ " :" + message
-		+ "\r\n";
-	for (std::set<int>::iterator it = _userFds.begin(); it != _userFds.end(); ++it)
+	_users.erase(user);
+	user->leaveChannel(this);
+}
+
+void Channel::addOperator(User *user)
+{
+	_operators.insert(user);
+}
+
+void Channel::removeOperator(User *user)
+{
+	_operators.erase(user);
+}
+
+void Channel::addInvitedUser(User *user)
+{
+	_invited.insert(user);
+}
+
+void Channel::removeInvitedUser(User *user)
+{
+	_invited.erase(user);
+}
+
+bool Channel::isUser(User *user) const
+{
+	return (_users.find(user) != _users.end());
+}
+
+bool Channel::isOperator(User *user) const
+{
+	return (_operators.find(user) != _operators.end());
+}
+
+bool Channel::isInvitedUser(User *user) const
+{
+	return (_invited.find(user) != _invited.end());
+}
+
+std::string Channel::getMembersList() const
+{
+	std::string membersList;
+	for (std::set<User *>::const_iterator it = _users.begin(); it != _users.end(); it++)
 	{
-		if (*it == excludeFd)
-			continue;
-		server.sendData(*it, fullMsg);
+		membersList += (*it)->getNickname();
+		membersList += " ";
+	}
+	return (membersList);
+}
+
+// Channel operations
+void Channel::broadcastAll(Server *server, std::string const &message) const
+{
+	for (std::set<User *>::const_iterator it = _users.begin(); it != _users.end(); it++)
+	{
+		server->sendData((*it)->getFd(), message);
 	}
 }
 
-void Channel::broadcastRaw(Server &server, const std::string &msg)
+void Channel::broadcast(Server *server, std::string const &message, int excludeFd) const
 {
-	for (std::set<int>::iterator it = _userFds.begin(); it != _userFds.end(); ++it)
+	for (std::set<User *>::const_iterator it = _users.begin(); it != _users.end(); it++)
 	{
-		server.sendData(*it, msg);
+		int	sd = (*it)->getFd();
+		if (sd != excludeFd)
+			server->sendData(sd, message);
 	}
-}
-
-
-
-void Channel::addInvited(const std::string &nickname) {
-    _invited.insert(nickname);
-
-	
-}
-bool Channel::isInvited(const std::string &nickname) const {
-    return (_invited.find(nickname) != _invited.end());
-}
-void Channel::removeInvited(const std::string &nickname) {
-    _invited.erase(nickname);
-}
-bool Channel::hasMode(int flag) const {
-    return ((_mode & flag) != 0);
 }
